937d0d3288224405fbdb7aaf7e18ee788490d62a
Quentin <quentin.moessner@bellman.immo>
2023-10-23 19:12:31 +0200

    feat: content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
@@ -12,32 +11,6 @@ export default defineConfig({
   output: "server",
   adapter: netlify(),
   integrations: [tailwind(), svelte(), mdx()],
-  redirects: {
-    "/legal": {
-      status: 302,
-      destination: "/incoming",
-    },
-    "/privacy": {
-      status: 302,
-      destination: "/incoming",
-    },
-    "/gallery": {
-      status: 302,
-      destination: "/incoming",
-    },
-    "/api/posts": {
-      status: 302,
-      destination: "/incoming",
-    },
-    "/api/reviews": {
-      status: 302,
-      destination: "/incoming",
-    },
-    "/": {
-      status: 302,
-      destination: "/incoming",
-    },
-  },
   vite: {
     ssr: {
       external: ["svgo"],
