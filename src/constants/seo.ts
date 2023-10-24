8815bb8f04fa0e303d4e5ecbbb1168efc97c79ad
Quentin <quentin.moessner@bellman.immo>
2023-10-24 15:03:35 +0200

    chore: seo improvments

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,5 +1,5 @@
 export const description =
-  'Votre coiffeur élégant sur le port de La Seyne-sur-Mer. SC-Coiffure, experts capillaires pour tous. Ambiance conviviale, tarifs exceptionnels. Réservez maintenant!';
+  'Votre coiffeur élégant à La Seyne-sur-Mer. SC-Coiffure, experts capillaires pour tous. Ambiance conviviale, tarifs exceptionnels. Réservez maintenant!';
 export const title = 'SC-Coiffure - Votre coiffeur à La Seyne-sur-Mer';
 
 export const seo = {
