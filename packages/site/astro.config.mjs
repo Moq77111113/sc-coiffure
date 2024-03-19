69ab6ccaf90a619626eb45621381f4ea7d11ca84
Quentin Moessner <quentin.moessner@gmail.com>
2024-03-19 10:36:32 +0100

    chore: update packages

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify/functions';
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sccoiffure83.fr',
   adapter: netlify(),
-  integrations: [tailwind(), svelte(), mdx()],
+  integrations: [tailwind(), svelte(), mdx(), icon()],
 
   vite: {
     ssr: {
