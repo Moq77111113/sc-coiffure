4ad46e21411cbe2850e55d739803514f79168eaf
Quentin Moessner <quentin.moessner@gmail.com>
2024-07-03 07:43:49 +0200

    wip: pkg

--- .prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    'prettier-plugin-astro',
-    'prettier-plugin-tailwindcss',
-    'prettier-plugin-svelte',
-  ],
-  pluginSearchDirs: false,
-  singleQuote: true,
-};
