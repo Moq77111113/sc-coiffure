fba80f451e93c6bcb875e0dec361ee174c90f32a
Quentin Moessner <quentin.moessner@gmail.com>
2025-09-20 08:24:20 +0200

    feat(prices): update amounts and correct spelling in service names (#21)

--- packages/site/src/constants/prices.ts
+++ packages/site/src/constants/prices.ts
@@ -5,20 +5,20 @@ const introPrices = [
   [
     {
       name: 'Shampooing brushing',
-      amounts: [1600, 2100, 2800],
+      amounts: [1900, 2100, 3100],
     },
 
     {
       name: 'Shampooing coupe brushing',
-      amounts: [2900, 3100, 3600],
+      amounts: [3200, 3400, 3900],
     },
     {
-      name: 'Shampooing coupe couleur avec amnoniaque',
-      amounts: [4500, 5500, 6500],
+      name: 'Shampooing coupe couleur avec ammoniaque',
+      amounts: [4800, 5800, 6800],
     },
     {
-      name: 'Shampooing coupe couleur sans amnoniaque',
-      amounts: [5200, 6200, 7200],
+      name: 'Shampooing coupe couleur sans ammoniaque',
+      amounts: [5800, 6500, 7500],
     },
     { name: 'Shampooing coupe balayage soin', amounts: [5900, 6900, 7900] },
   ],
